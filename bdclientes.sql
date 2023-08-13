02a14ff5233b099ce897549b6fff624f8a15ebf1
AngelMasterr <122641167+AngelMasterr@users.noreply.github.com>
2023-08-12 19:00:04 -0500

    creando base de datos simple desde Código

---
+++ bdclientes.sql
@@ -0,0 +1,15 @@
+USE JUGOS;
+create table TBCLIENTES3	(
+DNI varchar(20),
+NOMBRE varchar(150),
+DIRECCION1 varchar(150),
+DIRECCION2 varchar(150),
+BARRIO varchar(50),
+CIUDAD varchar(50),
+ESTADO varchar(50),
+CP varchar(10),
+EDAD smallint,
+SEXO varchar(1),
+LIMITE_CREDITO float,
+VOLUMEN_COMPRA float,
+PRIMERA_COMPRA bit(1));
